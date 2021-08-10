b7161c7c7df6bd2b14aa96e73fb1224f528c5890
InnocentEdem <82113948+InnocentEdem@users.noreply.github.com>
2021-08-10 10:45:40 +0000

    Initial

---
+++ whaleTalk.js
@@ -0,0 +1,19 @@
+let input = 'earmark';
+const vowels = ['a','e','i','u','o'];
+let resultArray = [];
+for(let i =0; i<input.length; i++){
+  //console.log(i);
+  for(let j = 0; j<vowels.length; j++){
+    if(input[i]===vowels[j]){
+      if(input[i]==='e'){
+        resultArray.push(input[i])
+      }
+      else if(input[i]==='u'){
+        resultArray.push(input[i])
+      }
+    }
+  }
+
+}
+
+console.log(resultArray.toString().toUpperCase())
